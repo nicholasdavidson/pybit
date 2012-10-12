d260beac6fa44e688c8181d86ac9991d7918ab21
codehelp <codehelp@cfba51da-52d0-41e7-b607-267b4448452c>
2012-10-12 09:54:18 +0000

    Migrate to python2.6 for easier exception handling.

---
+++ software/trunk/packages/PyBit/test/pybitclient-test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       pybitclient-test.py
+#
+#       Copyright 2012 Neil Williams <codehelp@debian.org>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+import unittest
+import pybitclient
+from pybitclient.debian import DebianBuildClient
+from pybitclient.subversion import SubversionClient
+#from pybitclient.buildclient import BuildClient
+
+class TestClient(unittest.TestCase) :
+	def setUp (self):
+		return
+
+	def test_build_client (self) :
+#		client = BuildClient()
+		deb_client = DebianBuildClient()
+
+if __name__ == '__main__':
+	unittest.main()
