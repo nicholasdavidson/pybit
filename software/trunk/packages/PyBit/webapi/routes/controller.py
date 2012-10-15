d330d035dd7f68dccfba9702ed0523736cd5cc63
simonh <simonh@cfba51da-52d0-41e7-b607-267b4448452c>
2012-10-15 14:10:23 +0000

    Further updates for controller.

--- software/trunk/packages/PyBit/webapi/routes/controller.py
+++ software/trunk/packages/PyBit/webapi/routes/controller.py
@@ -20,6 +20,8 @@ class controller:
 
 	def __init__(self, jobDb):
 		self.job_db = jobDb
+		self.conn = amqp.Connection(host="localhost:5672", userid="guest", password="guest", virtual_host="/", insist=False)
+		self.chan = self.conn.channel()
 		print "controller init"
 
 	def add(self):
@@ -39,6 +41,8 @@ class controller:
 			if not uri and method and dist and vcs_id and architectures and version and package_name and suite and format :
 				response.status = "400 - Required fields missing."
 				return
+			else :
+				print package_name, version, suite, architectures
 		except Exception as e:
 			raise Exception('Error parsing job information: ' + str(e))
 			response.status = "500 - Error parsing job information"
@@ -46,7 +50,7 @@ class controller:
 
 		try:
 			supported_arches = self.job_db.supportedArchitectures(suite)
-			print "DEBUG: ", supported_arches
+			print "SUPPORTED ARCHITECTURES: ", supported_arches
 
 			if (len(supported_arches) == 0):
 				response.status = "404 - no supported architectures for this suite."
@@ -67,7 +71,7 @@ class controller:
 					print "FOUND", len(matching_package_versions)
 					current_package = matching_package_versions[0]
 				else :
-					print "NO PACKAGE FOUND"
+					print "NO MATCHING PACKAGE FOUND"
 					# add new package to db
 					current_package = self.job_db.put_package(version,package_name)
 					if not current_package.id :
@@ -75,6 +79,8 @@ class controller:
 						print "FAILED TO ADD PACKAGE:", package_name
 						response.status = "404 - failed to add package."
 						return
+					else :
+						print "ADDED PACKAGE", current_package.id
 				current_suite_id = self.job_db.get_suite_byname(suite)[0].id
 				current_dist_id = self.job_db.get_dist_byname(dist)[0].id
 				current_format_id = self.job_db.get_format_byname(format)[0].id
@@ -84,15 +90,14 @@ class controller:
 						# add package instance to db
 						new_packageinstance = self.job_db.put_packageinstance(current_package.id,current_arch_id,current_suite_id,current_dist_id,current_format_id,False)
 						if new_packageinstance.id :
-							print "ADDED PackageInstance: ", new_packageinstance.id
 							# TODO: check if database contains a package where status = building, version < package_version, suite = suite
 							new_job = self.job_db.put_job(new_packageinstance.id, None)
 							if new_job.id :
-								print "ADDED Job:", new_job.id, " for ", arch
+								print "ADDED Job:", new_job.id, "PackageInstance:", new_packageinstance.id, "for", arch
 								#TODO: add job to message queue
-	#							msg = amqp.Message(jsonpickle.encode(instance))
-	#							msg.properties["delivery_mode"] = 2
-	#							chan.basic_publish(msg,exchange="i386",routing_key="buildd")
+								msg = amqp.Message(jsonpickle.encode(new_job))
+								msg.properties["delivery_mode"] = 2
+								self.chan.basic_publish(msg,exchange="i386",routing_key="buildd")
 							else :
 								print "FAILED TO ADD JOB"
 								response.status = "404 - failed to add job."
