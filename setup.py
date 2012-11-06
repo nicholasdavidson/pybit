86a10318ff7988fe1b8d66a61f06a8dc71878307
Neil Williams <codehelp@debian.org>
2012-11-06 17:34:16 +0000

    Move up to a version higher than the released tag. Include newly added pybitweb directory.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@
 from setuptools import setup
 
 setup(name='pybit',
-	version='0.1.0',
+	version='0.1.1',
 	description='PyBit buildd integrated toolkit',
 	license='gpl2',
 	url='https://github.com/nicholasdavidson/pybit.git',
