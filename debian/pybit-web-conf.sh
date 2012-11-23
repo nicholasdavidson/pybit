414f69d2f4f3eab1d744b9894d1f473946e21c0c
Neil Williams <codehelp@debian.org>
2012-11-23 09:17:07 +0000

    convert the dbconfig call to a snippet to avoid it being called accidentally.

--- debian/pybit-web-conf.sh
+++ debian/pybit-web-conf.sh
@@ -1,19 +1,17 @@
-#!/bin/sh
-
-set -e
-
+# shell snippet to get the dbconfig values for pybit-web
+# as there is no perl binding for dbconfig.
+# Used by pybit-web.postinst
+# idempotency - this is an intermediate file only.
 ucf -p /etc/pybit/debian-db.sh
-# postinst maintainer script for foo-mysql
-
 # source debconf stuff
 . /usr/share/debconf/confmodule
 # source dbconfig-common stuff
-. /usr/share/dbconfig-common/dpkg/postinst.mysql
+. /usr/share/dbconfig-common/dpkg/postinst.pgsql
 
 # create an output file which ucf will watch
 # (and ensure isn't recreated) until we need to do it
 dbc_generate_include=perl:/etc/pybit/debian-db.pl
+# minimal permissions
 dbc_generate_include_owner="root:root"
 dbc_generate_include_perms="600"
-
-dbc_go foo-mysql $@
+dbc_go pybit-web $@
