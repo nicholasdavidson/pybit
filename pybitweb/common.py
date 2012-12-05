d80a6d0b943e2790b1779d9e391b968e610f0a36
jamesbennet <github@james-bennet.com>
2012-12-05 13:05:50 +0000

    Fixed the fix from 2b8f5c6198a48f7851c90f853c153e919f5e34b9 so it uses the non-obsolete load_settings code, the old buggy code, which was a dupe, is now removed from pybitweb/common.py

--- pybitweb/common.py
+++ pybitweb/common.py
@@ -25,6 +25,8 @@
 import jsonpickle
 import os
 import sys
+import pybit
+
 from os.path import isfile
 from pybitweb.bottle import Bottle,request,response
 
@@ -37,12 +39,10 @@ def checkValue(value,container):
 # TODO: This is a huge bodge. Query the DB for this!
 def check_auth(username, password):
 	# Load from local settings file in configs, or if not, from system settings in etc.
-	auth_settings = load_from_cwd("web/web.conf")
+	(auth_settings,path) = pybit.load_settings("web/web.conf")
+	print str(auth_settings)
 	if not auth_settings:
 		# Cant load settings
-		auth_settings = load_from_etc("web.conf")
-	if not auth_settings:
-		# Still cant load settings
 		return False
 
 	# Check credentials
@@ -68,69 +68,3 @@ def requires_auth(f):
 		else:
 			return f(*args, **kwargs)
 	return decorated
-
-def load_from_cwd(filename):
-	try:
-		#print "DEBUG: Opening local file" , filename
-		path = "%s/configs/%s" % (os.getcwd(),filename);
-
-		if isfile(path): # exists
-			settings_file = open(path, 'r')
-			if settings_file: # can actually open it
-				encoded_string = settings_file.read()
-				settings = jsonpickle.decode(encoded_string )
-				#print "DEBUG: Opened local config file", path
-				return settings
-			else:
-				#print "ERROR: Error loading local file: ", path
-				return None
-		else:
-			#print "ERROR: Error loading local file from path: ", path
-			return None
-	except Exception as e:
-			raise Exception('Error loading local file:' + filename + str(e))
-			return None
-
-def load_from_etc(filename):
-	try:
-		#print "DEBUG: Opening config file" , filename
-		path = "/etc/pybit/web/%s" % (filename);
-
-		if isfile(path): # exists
-			settings_file = open(path, 'r')
-			if settings_file: # can actually open it
-				encoded_string = settings_file.read()
-				settings = jsonpickle.decode(encoded_string )
-				#print "DEBUG: Opened config file", path
-				return settings
-			else:
-				#print "ERROR: Error loading config file: ", path
-				return None
-		else:
-			#print "ERROR: Error loading config file from path: ", path
-			return None
-	except Exception as e:
-			raise Exception('Error loading config file:' + filename + str(e))
-			return None
-
-def load_settings_from_file(filename):
-	try:
-		print "DEBUG: Loading settings file:" , filename, "....."
-		localsettings = load_from_cwd(filename)
-		globalsettings = load_from_etc(filename)
-
-		#print "Local settings: " + repr(localsettings) + "Global settings: " + repr(globalsettings)
-
-		if localsettings:
-			print "DEBUG: Using local settings file"
-			return localsettings
-		elif globalsettings:
-			print "DEBUG: Using system settings file"
-			return globalsettings
-		else:
-			raise Exception # not loading settings is fatal, bail out
-			return None
-	except Exception as e:
-			print ("FATAL ERROR: Error loading settings from: " + filename)
-			sys.exit(-1)
-			return False
