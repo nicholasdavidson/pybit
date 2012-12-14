c66070b30e2d9f23843a03eb4533967c270c5ab2
Neil Williams <codehelp@debian.org>
2012-12-14 12:29:54 +0000

    Prepare for a new upstream release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@
 from setuptools import setup
 
 setup(name='pybit',
-	version='0.3.0',
+	version='0.4.0',
 	description='PyBit buildd integrated toolkit',
 	license='gpl2',
 	url='https://github.com/nicholasdavidson/pybit.git',
