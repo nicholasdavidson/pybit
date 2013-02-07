21103627a6bd2654032fab5d91687325c2e37093
jamesbennet <github@james-bennet.com>
2013-02-07 14:01:32 +0000

    Fixed syntax error in v5.sql update script

--- db/updates/v5.sql
+++ db/updates/v5.sql
@@ -2,8 +2,9 @@ create or replace function run_update() returns void as
 $$
 begin
 if not exists (SELECT * FROM information_schema.tables WHERE
-		table_catalog = CURRENT_CATALOG AND table_schema = CURRENT_SCHEMA
-			ALTER TABLE PackageInstance ADD buildenv_id;
+		table_catalog = CURRENT_CATALOG AND table_schema = CURRENT_SCHEMA)
+then
+			ALTER TABLE PackageInstance ADD COLUMN buildenv_id bigint;
 			UPDATE schema_version SET id=5;
 end if;
 end;
