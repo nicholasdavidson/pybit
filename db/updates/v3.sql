55a9817d3803542b515702e1ddb5be63cfa30060
Nicholas Davidson <nicholas.davidson@toby-churchill.com>
2013-02-12 14:50:03 +0000

    switched version upgrade schemas to check schema_version table. Except v1 obviously.

--- db/updates/v3.sql
+++ db/updates/v3.sql
@@ -20,18 +20,17 @@ DROP FUNCTION make_plpgsql();
 create or replace function run_update() returns void as
 $$
 begin
-if not exists (SELECT * FROM information_schema.tables WHERE
-		table_catalog = CURRENT_CATALOG AND table_schema = CURRENT_SCHEMA
-		AND table_name = 'buildrequest') then
-			CREATE TABLE BuildRequest (
-				id SERIAL PRIMARY KEY NOT NULL,
-				job bigint NOT NULL,
-				method text NOT NULL,
-				uri text NOT NULL,
-				vcs_id text NOT NULL,
-				FOREIGN KEY (job) REFERENCES Job (id)
-			);
-			UPDATE schema_version SET id=3;
+if (SELECT count(*) from schema_version WHERE id=2)
+then
+	CREATE TABLE BuildRequest (
+		id SERIAL PRIMARY KEY NOT NULL,
+		job bigint NOT NULL,
+		method text NOT NULL,
+		uri text NOT NULL,
+		vcs_id text NOT NULL,
+		FOREIGN KEY (job) REFERENCES Job (id)
+	);
+	UPDATE schema_version SET id=3;
 end if;
 end;
 $$ LANGUAGE plpgsql;
