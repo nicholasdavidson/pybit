a6fc969fca59b7ddfad8b2538aa850050d84ca3e
jamesbennet <github@james-bennet.com>
2013-02-14 11:50:16 +0000

    Fixed populate.sql to be more useful and up to date, inserts build envs and arch weights now

--- db/populate.sql
+++ db/populate.sql
@@ -1,6 +1,5 @@
 INSERT INTO arch (name) VALUES
 	('armel'),
-	('armel-cross'),
 	('i386');
 
 INSERT INTO distribution(name) VALUES
@@ -8,67 +7,56 @@ INSERT INTO distribution(name) VALUES
 
 INSERT INTO suite(name) VALUES
 	('chickpea'),
-	('development');
+	('development'),
+	('illgill');
 
 INSERT INTO format(name) VALUES
 	('deb');
 
-INSERT INTO package(name, version) VALUES
-	('logicalmodel','0.4.42'),
-	('logicalmodel','0.4.39chickpea4');
-
-INSERT INTO packageinstance(package_id, arch_id, suite_id, dist_id, format_id) VALUES ( 
-	(SELECT id FROM package WHERE name='logicalmodel' AND version='0.4.39chickpea4'),
+INSERT INTO suitearches(suite_id, arch_id, master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='development'),
 	(SELECT id FROM arch WHERE name='armel'),
-	(SELECT id FROM suite WHERE name='chickpea'),
-	(SELECT id FROM distribution WHERE name='Debian'),
-	(SELECT id FROM format WHERE name='deb'));
+	0
+);
 
- INSERT INTO packageinstance(package_id, arch_id, suite_id, dist_id, format_id) VALUES (
-	(SELECT id FROM package WHERE name='logicalmodel' AND version='0.4.39chickpea4'),
+INSERT INTO suitearches(suite_id, arch_id,master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='development'),
 	(SELECT id FROM arch WHERE name='i386'),
- 	(SELECT id FROM suite WHERE name='chickpea'),
-	(SELECT id FROM distribution WHERE name='Debian'),
-	(SELECT id FROM format WHERE name='deb'));
+	1
+);
 
-INSERT INTO packageinstance(package_id, arch_id, suite_id, dist_id, format_id) VALUES (
-	(SELECT id FROM package WHERE name='logicalmodel' AND version='0.4.42'),
+INSERT INTO suitearches(suite_id, arch_id,master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='chickpea'),
 	(SELECT id FROM arch WHERE name='armel'),
-	(SELECT id FROM suite WHERE name='development'),
-	(SELECT id FROM distribution WHERE name='Debian'),
-	(SELECT id FROM format WHERE name='deb'));
+	0
+);
 
-INSERT INTO packageinstance(package_id, arch_id, suite_id, dist_id, format_id) VALUES (
-	(SELECT id FROM package WHERE name='logicalmodel' AND version='0.4.42'),
+INSERT INTO suitearches(suite_id, arch_id,master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='chickpea'),
 	(SELECT id FROM arch WHERE name='i386'),
-	(SELECT id FROM suite WHERE name='development'),
-	(SELECT id FROM distribution WHERE name='Debian'),
-	(SELECT id FROM format WHERE name='deb'));
-
-INSERT INTO suitearches(suite_id, arch_id) VALUES (
-	(SELECT id FROM suite WHERE name='development'),
-	(SELECT id FROM arch WHERE name='armel')
+	1
 );
 
-INSERT INTO suitearches(suite_id, arch_id) VALUES (
-	(SELECT id FROM suite WHERE name='development'),
-	(SELECT id FROM arch WHERE name='i386')
+INSERT INTO suitearches(suite_id, arch_id,master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='illgill'),
+	(SELECT id FROM arch WHERE name='armel'),
+	0
 );
 
-INSERT INTO suitearches(suite_id, arch_id) VALUES (
-	(SELECT id FROM suite WHERE name='chickpea'),
-	(SELECT id FROM arch WHERE name='armel')
+INSERT INTO suitearches(suite_id, arch_id,master_weight) VALUES (
+	(SELECT id FROM suite WHERE name='illgill'),
+	(SELECT id FROM arch WHERE name='i386'),
+	1
 );
 
-INSERT INTO suitearches(suite_id, arch_id) VALUES (
-	(SELECT id FROM suite WHERE name='chickpea'),
-	(SELECT id FROM arch WHERE name='i386')
-);
+INSERT INTO buildenv(name) VALUES ('squeeze');
+INSERT INTO buildenv(name) VALUES ('wheezy');
 
 INSERT INTO buildclients(name) VALUES 
 	('arm01'),
 	('arm02'),
 	('buildbox');
+
 INSERT INTO status(name) VALUES
 	('Waiting'),
 	('Blocked'),
