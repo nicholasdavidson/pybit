e9fa2338b9ac6807f3ea828193e864f5b7d36f95
Neil Williams <codehelp@debian.org>
2013-05-18 23:34:25 +0100

    Push to version 1.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@
 from setuptools import setup
 
 setup(name='pybit',
-    version='0.5.0',
+    version='1.0.0',
     description='PyBit buildd integrated toolkit',
     license='gpl2',
     url='https://github.com/nicholasdavidson/pybit.git',
