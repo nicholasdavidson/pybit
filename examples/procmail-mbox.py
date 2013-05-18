cbdbe52ae74c3de0c9ff8ad804e74666fea98276
Neil Williams <codehelp@debian.org>
2013-05-18 17:28:51 +0100

    make the comments more useful

--- examples/procmail-mbox.py
+++ examples/procmail-mbox.py
@@ -2,28 +2,31 @@
 # -*- coding: utf-8 -*-
 #
 #  procmail-mbox.py
-#  
+#
 #  Copyright 2013 Neil Williams <codehelp@debian.org>
-#  
+#  Copyright 2002 Noah Spurrier
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+
+#  Derived from http://code.activestate.com/recipes/157437-reading-and-writing-mbox-style-mailbox-files/
 
 # The purpose of this file is to emulate procmail handling for a
-# subscription to debian-devel-changes. Pass a standard mbox 
-# as the first argument to the script. e.g.
+# subscription to debian-devel-changes to help testing of the
+# changes-debian hook. Pass a standard mbox as the first argument
+# to the script. e.g.
 # ./procmail-mbox.py mbox
 # This example is hard-coded to use the changes-debian hook
 # in ../hook/
@@ -33,26 +36,23 @@ import sys, os, string, re
 from subprocess import Popen, PIPE, STDOUT
 
 def passthrough_filter (msg, document):
-    """This prints the 'from' address of the message and
-    returns the document unchanged.
+    """If you want to extend this to act as a filter
+    on the changes file being parsed, simply return None here.
+    msg contains the full email, with headers.
+    document is just the .changes file.
     """
     return document
 
 def process_mailbox (mailboxname_in, filter_function):
-    """This processes a each message in the 'in' mailbox and optionally
-    writes the message to the 'out' mailbox. Each message is passed to
-    the  filter_function. The filter function may return None to ignore
-    the message or may return the document to be saved in the 'out' mailbox.
-    See passthrough_filter().
-    """
+    """This processes a each message in the 'in' mailbox. Each message
+    is passed to the filter_function."""
     # Open the mailbox.
     mb = mailbox.UnixMailbox (file(mailboxname_in,'r'))
 
     msg = mb.next()
     while msg is not None:
         # Properties of msg cannot be modified, so we pull out the
-        # document to handle is separately. We keep msg around to
-        # keep track of headers and stuff.
+        # document to handle it separately.
         document = msg.fp.read()
         document = filter_function (msg, document)
         if document is not None:
